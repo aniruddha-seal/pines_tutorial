644ecbcbe0cfab565a5cc5eebf49e2a1ff128060
Aniruddha Seal <aniruddhaseal@midway3-login3.rcc.local>
2024-10-21 15:19:59 -0500

    clean input

--- data/0.unbiased/4_calcPIV/calcPIV.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-plumed driver --plumed plumed_calcPIV_newSFP_piv91.dat --mf_xtc md_whole.xtc >& out.out
-
-sed -i '/#END OF FRAME/d' PINES_representation_traj.dat
-
-python3 genNPYfile.py
--- data/0.unbiased/4_calcPIV/genNPYfile.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import pandas as pd
-PIVs = pd.read_csv("PINES_representation_traj.dat", delim_whitespace="tab", header=None)
-np.save("NaCl_PIVs_round_0.npy", PIVs)
